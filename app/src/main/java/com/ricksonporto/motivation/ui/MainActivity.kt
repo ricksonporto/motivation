c04cab49748f3e1e5c626fa99be0c1821b2cb45f
ricksonporto <rickson.porto@hotmail.com>
2023-04-15 02:15:16 -0300

    botões da MainActivity agora mudam de cor ao serem clicados

--- app/src/main/java/com/ricksonporto/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/ricksonporto/motivation/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ricksonporto.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.ricksonporto.motivation.R
 import com.ricksonporto.motivation.infra.MotivationConstants
 import com.ricksonporto.motivation.infra.SecurityPreferences
 import com.ricksonporto.motivation.databinding.ActivityMainBinding
@@ -18,12 +20,32 @@ class MainActivity : AppCompatActivity() {
         //Esconder barra
         supportActionBar?.hide()
         handleUserName()
+        handleFilter(R.id.image_all)
     }
 
     //Eventos
     private fun onClick() {
-        binding?.buttonNewPhrase?.setOnClickListener {
+        binding?.buttonNewPhrase?.setOnClickListener {}
+        binding?.imageAll?.setOnClickListener {}
+        binding?.imageHappy?.setOnClickListener {}
+        binding?.imageSunny?.setOnClickListener {}
+    }
 
+    private fun handleFilter(id: Int) {
+        binding?.imageAll?.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding?.imageHappy?.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding?.imageSunny?.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding?.imageAll?.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_happy -> {
+                binding?.imageHappy?.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
+            R.id.image_Sunny -> {
+                binding?.imageSunny?.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            }
         }
     }
 
